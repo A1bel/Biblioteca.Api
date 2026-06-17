dbe1947d4a73aadc1579073d14929f1c23cf37fb
Arthur Aibel <Arthur.Aibel1@gmail.com>
2026-06-16 23:03:43 -0300

    chore: add initial project files

---
+++ Biblioteca.Api/Database/DatabaseAccess.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace Biblioteca.Api.Database
+{
+    public class DatabaseAccess
+    {
+        private readonly string _connectionString;
+
+        public DatabaseAccess(IConfiguration configuration)
+        {
+            _connectionString =
+                configuration.GetConnectionString("Biblioteca");
+        }
+
+        public SqlConnection OpenConnection()
+        {
+            SqlConnection connection =
+                new SqlConnection(_connectionString);
+
+            connection.Open();
+
+            return connection;
+        }
+    }
+}
