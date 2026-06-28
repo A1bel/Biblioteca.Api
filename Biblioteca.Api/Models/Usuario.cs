ec4beae8736817f3068cfe7c1161a75c1a4566e1
Arthur Aibel <Arthur.Aibel1@gmail.com>
2026-06-28 16:54:23 -0300

    feat: add user model

---
+++ Biblioteca.Api/Models/Usuario.cs
@@ -0,0 +1,14 @@
+﻿namespace Biblioteca.Api.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public int IdPerfil { get; set; }
+        public string Perfil { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }  
+
+    }
+}
