d53b53c439455e3d7d91640ad865c8dfd69b2c1a
Arthur Aibel <Arthur.Aibel1@gmail.com>
2026-06-28 18:53:47 -0300

    feat: add user registration DTO and validation rules

---
+++ Biblioteca.Api/DTOs/UsuarioCreateRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace Biblioteca.Api.DTOs
+{
+    public class UsuarioCreateRequest
+    {
+        public int IdPerfil { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public string ConfirmarSenha { get; set; }
+    }
+}
---
+++ Biblioteca.Api/Validators/UsuarioValidator.cs
@@ -0,0 +1,59 @@
+﻿using Biblioteca.Api.DTOs;
+using System.Text.RegularExpressions;
+
+namespace Biblioteca.Api.Validators
+{
+    public class UsuarioValidator
+    {
+        public Dictionary<string, string> ValidateCreate(UsuarioCreateRequest usuario)
+        {
+            Dictionary<string, string> errors = [];
+
+            if (usuario.IdPerfil != 1 && usuario.IdPerfil != 2)
+                errors.Add("idPerfil", "Perfil inválido");
+
+            if (string.IsNullOrEmpty(usuario.Nome))
+                errors.Add("nome", "O nome é obrigatório");
+
+            if (string.IsNullOrEmpty(usuario.Cpf))
+                errors.Add("cpf", "O cpf é obrigatório");
+            else
+            {
+                if (usuario.Cpf.Length != 11)
+                    errors.Add("cpf", "O cpf deve conter 11 dígitos");
+                else if (!usuario.Cpf.All(char.IsDigit))
+                    errors.Add("cpf", "O cpf deve conter apenas números");
+            }
+
+            if (string.IsNullOrEmpty(usuario.Email))
+                errors.Add("email", "O email é obrigatório");
+            else
+            {
+                if (!Regex.IsMatch(usuario.Email,
+                @"^[^@\s]+@[^@\s]+\.[^@\s]+$"))
+                {
+                    errors.Add("email", "E-mail inválido");
+                }
+            }
+
+            if (string.IsNullOrEmpty(usuario.Senha))
+                errors.Add("senha", "A senha é obrigatória");
+            else if (usuario.Senha.Length < 6)
+                errors.Add("senha", "A senha deve possuir no mínimo 6 caracteres");
+
+            if (string.IsNullOrEmpty(usuario.ConfirmarSenha))
+                errors.Add("confirmarSenha", "A confirmação senha é obrigatória");
+            else if (usuario.ConfirmarSenha.Length < 6)
+                errors.Add("confirmarSenha", "A senha deve possuir no mínimo 6 caracteres");
+
+            if (!errors.ContainsKey("senha") &&
+                !errors.ContainsKey("confirmarSenha") &&
+                usuario.Senha != usuario.ConfirmarSenha)
+            {
+                errors.Add("confirmarSenha", "As senhas não coincidem");
+            }
+
+            return errors;
+        }
+    }
+}
